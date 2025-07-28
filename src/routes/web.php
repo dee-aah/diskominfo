a02f672d91acbe2fdbcae921b7a957237ea37f3e
dee-aah <dedeahmadmaolana4@gmail.com>
2025-07-28 16:57:21 +0700

    insert visi misi and  tupoksi

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\berandaController;
+use App\Http\Controllers\tupoksiController;
+use App\Http\Controllers\visimisiController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/beranda', [berandaController::class, 'index']);
+Route::resource(name: '/visimisi',  controller: visimisiController::class);
+Route::resource(name: '/tupoksi',  controller: tupoksiController::class);
